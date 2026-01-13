8c503db1a988f150f1b4a29dbeff0ac0efb64cc1
Ângelo <familiasabino14@gmail.com>
2026-01-13 11:10:42 -0300

    feat: criação do arquivo inicial server.js e teste de execução simultânea

---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * --- BACK-END INICIAL PEDEAÍ ---
+ */
+console.log("-----------------------------------------");
+console.log("🚀 BACK-END: Servidor Node.js iniciado!");
+console.log("📡 Aguardando conexões...");
+console.log("-----------------------------------------");
+
+// Por enquanto, apenas um log para testar a execução simultânea
+setInterval(() => {
+  console.log("[Back-end] Sincronizando dados com o sistema...");
+}, 10000);
